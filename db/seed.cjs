01ba36b5ec143b19c4431a0a09ccb3ec9b3f0062
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-04 15:02:00 -0400

    Create flavors table

--- db/seed.cjs
+++ db/seed.cjs
@@ -1,1 +1,41 @@
 const client = require('./client.cjs');
+
+const dropTables = async() => {
+  try {
+    await client.query(`DROP TABLE IF EXISTS flavors;`)
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+const createTables = async() => {
+  try {
+    await client.query(`
+      CREATE TABLE flavors (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20) NOT NULL UNIQUE,
+        is_favorite BOOLEAN,
+        created_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP NOT NULL
+      );
+    `)
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+const syncAndSeed = async() => {
+  await client.connect();
+  console.log(`connected`);
+
+  await dropTables();
+  console.log(`dropped tables`);
+
+  await createTables();
+  console.log(`created tables`);
+
+  await client.end();
+  console.log(`disconnected`);
+}
+
+syncAndSeed();
